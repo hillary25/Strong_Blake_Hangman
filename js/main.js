66d3bfb5f153c8d40823b208eac5e57c6828aad3
hillary25 <35806842+hillary25@users.noreply.github.com>
2018-02-22 09:55:15 -0500

    New changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-// JavaScript Document
+(() => {
+console.log('hangman script friend');
 
-console.log("Linked up");
+  //create an array to hold the words to be guessed (MDN -> arrays, MDN const)
+  const words = ["blue", "orange", "yellow", "magenta", "violet"];
+
+  //set up a variable stack
+  let initButton = document.querySelector('button');//this is the new document.getElementById, now use querySelector('all')
+
+  function init() {
+    debugger;
+    console.log(words[3]);
+  }
+
+  //event handling always goes at the bottom
+  initButton.addEventListener('click', init);
+
+  // init();
+})();
