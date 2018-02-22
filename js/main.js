7ddf240935993f3d501882147746822e14eab70c
hillary25 <35806842+hillary25@users.noreply.github.com>
2018-02-22 10:45:54 -0500

    Changed JS and HTML
    End of class changes (Feb. 22)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 (() => {
-console.log('hangman script friend');
+console.log('hangman script fired!');
 
   //create an array to hold the words to be guessed (MDN -> arrays, MDN const)
   const words = ["blue", "orange", "yellow", "magenta", "violet"];
 
   //set up a variable stack
-  let initButton = document.querySelector('button');//this is the new document.getElementById, now use querySelector('all')
+  let currentWord; null,
+    wordHint = document.querySelector('.hint-string'),
+    guessBox = document.querySelector('input');
+
+  function takeGuess() {
+    //MDN -> 'this' keyword -'this' is whatever you and interacting with at the time and is firing off
+    console.log(this.value);
+
+    //if there is no letter, exit the game loop -> MDN 'or'
+    if (this.value == "" || this.value.length < 1 ){
+      return;
+    }
+  }
 
   function init() {
-    debugger;
-    console.log(words[3]);
+    //look at MDN -> the Math object
+    currentWord = words[Math.floor(Math.random()*words.length)];
+
+    //fill the hint with underscores -> MDN string methods / array .map method
+    wordHint.textContent = currentWord.split("").map(letter => letter = "__").join(" "); //this splits each letter and makes a underscore in their place, joined back together in the end
+
+    //MDN -> template string
+    console.log(`guess this word: ${currentWord}. It's at ${ words.indexOf(currentWord)}`);//varible into the string, $ is for plugging data in and making it dynamic
   }
 
   //event handling always goes at the bottom
-  initButton.addEventListener('click', init);
+  // initButton.addEventListener('click', init);
+  guessBox.addEventListener('keyup', takeGuess);
 
-  // init();
+  init();
 })();
